fd7b428934f6896749f6dcfd2f3030544ea3a2e9
Kent Martin Åsvang <kent@ebsdigital.no>
2022-07-03 15:23:20 +0200

    adding libs to tsconfig

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,14 @@
-var helloWorld = "hello world";
-function hello() {
-    console.log("hello there");
+"use strict";
+exports.__esModule = true;
+var core = require("@actions/core");
+try {
+    var host = core.getInput("host");
+    var user = core.getInput("user");
+    var password = core.getInput("password");
+    console.log("host: ".concat(host));
+    console.log("user: ".concat(user));
+    console.log("password: ".concat(password));
+}
+catch (error) {
+    core.setFailed(error.message);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
+import core = require("@actions/core");
+import github = require("@actions/github");
 
-let helloWorld: string = "hello world";
 
-function hello() { 
-    console.log("hello there");
+try 
+{
+    const host: string = core.getInput("host");
+    const user: string = core.getInput("user");
+    const password: string = core.getInput("password");
+
+    console.log(`host: ${host}`);
+    console.log(`user: ${user}`);
+    console.log(`password: ${password}`);
+} 
+catch (error) 
+{
+    core.setFailed(error.message); 
 }
